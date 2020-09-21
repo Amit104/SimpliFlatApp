0c9ecf06d62d9c9b37a521ef3e912877df120489
Amit Wadatkar <amwadatk@microsoft.com>
2020-09-21 20:17:14 +0530

    sign up pages UI

--- simpliflat/lib/screens/signup/signUpName.dart
+++ simpliflat/lib/screens/signup/signUpName.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:simpliflat/screens/signup/create_or_join.dart';
-import 'package:simpliflat/screens/signup/signupBackground.dart';
 import 'package:simpliflat/screens/utility.dart';
 import 'package:simpliflat/screens/home.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'dart:math';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:simpliflat/screens/widgets/common.dart';
 
 class SignUpName extends StatefulWidget {
   final phone;
@@ -36,159 +35,186 @@ class _SignUpNameUser extends State<SignUpName> {
     var deviceSize = MediaQuery.of(context).size;
 
     return Scaffold(
-        appBar: AppBar(title: Text("Name please"), elevation: 0.0),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text(
+            "ENTER NAME",
+            style: TextStyle(
+              color: Color(0xff373D4C),
+              fontFamily: 'Roboto',
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          elevation: 0.0,
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: Color(0xff373D4C),
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ),
         //resizeToAvoidBottomPadding: false,
-        body: Stack(children: <Widget>[
-          SignUpBackground(3),
-          Builder(builder: (BuildContext scaffoldContext) {
-            _scaffoldContext = scaffoldContext;
-            return Form(
-                key: _formKey,
-                child: Padding(
-                  padding: EdgeInsets.only(
-                      top: 50.0, left: _minpad * 2, right: _minpad * 2),
-                  child: ListView(
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.all(_minpad),
-                        child: Opacity(
-                          //
-                          opacity: 1,
-                          child: SizedBox(
-                              height: max(deviceSize.height / 2, 270),
-                              width: deviceSize.width * 0.85,
-                              child: new Card(
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius:
-                                          BorderRadius.circular(10.0)),
-                                  color: Colors.white,
-                                  elevation: 2.0,
-                                  child: Column(children: <Widget>[
-                                    Padding(
-                                      padding: EdgeInsets.only(
-                                        top: 60.0,
+        body: Builder(builder: (BuildContext scaffoldContext) {
+          _scaffoldContext = scaffoldContext;
+          return Form(
+              key: _formKey,
+              child: Padding(
+                padding: EdgeInsets.only(
+                  top: 60.0,
+                ),
+                child: ListView(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.all(_minpad),
+                      child: Opacity(
+                        //
+                        opacity: 1,
+                        child: SizedBox(
+                          width: deviceSize.width,
+                          child: new Card(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0)),
+                            color: Colors.white,
+                            elevation: 0.0,
+                            child: Column(
+                              children: <Widget>[
+                                Row(
+                                  children: <Widget>[
+                                    Expanded(
+                                      flex: 1,
+                                      child: Container(),
+                                    ),
+                                    Expanded(
+                                      flex: 7,
+                                      child: Text(
+                                        "Please enter your name",
+                                        style: TextStyle(
+                                            color: Colors.black,
+                                            fontFamily: 'Roboto',
+                                            fontWeight: FontWeight.w700,
+                                            fontSize: 18.0),
                                       ),
                                     ),
-                                    Row(
-                                      children: <Widget>[
-                                        Expanded(
-                                          flex: 1,
-                                          child: Container(),
+                                    Expanded(
+                                      flex: 1,
+                                      child: Container(),
+                                    ),
+                                  ],
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(
+                                    top: 35.0,
+                                  ),
+                                ),
+                                Row(
+                                  children: <Widget>[
+                                    Expanded(
+                                      flex: 1,
+                                      child: Container(),
+                                    ),
+                                    Expanded(
+                                      flex: 7,
+                                      child: TextFormField(
+                                        autofocus: true,
+                                        keyboardType: TextInputType.text,
+                                        style: TextStyle(
+                                          color: Colors.black,
+                                          fontFamily: 'Roboto',
+                                          fontWeight: FontWeight.w700,
+                                          fontSize: 30.0,
                                         ),
-                                        Expanded(
-                                          flex: 7,
-                                          child: Text(
-                                            "What should I call you?",
-                                            style: TextStyle(
-                                                color: Colors.black,
-                                                fontFamily: 'Montserrat',
-                                                fontSize: 18.0),
+                                        controller: name,
+                                        validator: (String value) {
+                                          if (value.isEmpty)
+                                            return "Please enter Name";
+                                          return null;
+                                        },
+                                        onFieldSubmitted: (v) {
+                                          _submitForm();
+                                        },
+                                        decoration: InputDecoration(
+                                          //labelText: "Name",
+                                          hintText: "Rahul",
+                                          //labelStyle: TextStyle(
+                                          //    color: Colors.white),
+                                          hintStyle:
+                                              TextStyle(color: Colors.grey),
+                                          errorStyle: TextStyle(
+                                            color: Colors.red,
+                                            fontSize: 12.0,
+                                            fontFamily: 'Roboto',
+                                            fontWeight: FontWeight.w700,
+                                          ),
+                                          border: new UnderlineInputBorder(
+                                            borderSide: new BorderSide(
+                                                color: Colors.black),
+                                          ),
+                                          focusedBorder:
+                                              new UnderlineInputBorder(
+                                            borderSide: new BorderSide(
+                                                color: Colors.black),
                                           ),
                                         ),
-                                        Expanded(
-                                          flex: 1,
-                                          child: Container(),
-                                        ),
-                                      ],
-                                    ),
-                                    Padding(
-                                      padding: EdgeInsets.only(
-                                        top: 35.0,
                                       ),
                                     ),
-                                    Row(
-                                      children: <Widget>[
-                                        Expanded(
-                                          flex: 1,
-                                          child: Container(),
-                                        ),
-                                        Expanded(
-                                          flex: 7,
-                                          child: TextFormField(
-                                            autofocus: true,
-                                            keyboardType: TextInputType.text,
-                                            style: TextStyle(
-                                                color: Colors.black,
-                                                fontFamily: 'Montserrat',
-                                                fontSize: 30.0,
-                                                fontWeight: FontWeight.w700),
-                                            controller: name,
-                                            validator: (String value) {
-                                              if (value.isEmpty)
-                                                return "Please enter Name";
-                                              return null;
-                                            },
-                                            onFieldSubmitted: (v) {
-                                              _submitForm();
-                                            },
-                                            decoration: InputDecoration(
-                                                //labelText: "Name",
-                                                hintText: "Rahul",
-                                                //labelStyle: TextStyle(
-                                                //    color: Colors.white),
-                                                hintStyle: TextStyle(
-                                                    color: Colors.grey),
-                                                errorStyle: TextStyle(
-                                                    color: Colors.red,
-                                                    fontSize: 12.0,
-                                                    fontFamily: 'Montserrat',
-                                                    fontWeight:
-                                                        FontWeight.w700),
-                                                border: InputBorder.none),
-                                          ),
-                                        ),
-                                        Expanded(
-                                          flex: 1,
-                                          child: Container(),
-                                        ),
-                                      ],
+                                    Expanded(
+                                      flex: 1,
+                                      child: Container(),
                                     ),
-                                    Padding(
-                                      padding: EdgeInsets.only(top: 30.0),
+                                  ],
+                                ),
+                                Padding(
+                                  padding: EdgeInsets.only(top: 30.0),
+                                ),
+                                Row(
+                                  children: <Widget>[
+                                    Expanded(
+                                      flex: 1,
+                                      child: Container(),
                                     ),
-                                    Row(
-                                      children: <Widget>[
-                                        Expanded(
-                                          flex: 1,
-                                          child: Container(),
-                                        ),
-                                        Expanded(
-                                          flex: 4,
-                                          child: ButtonTheme(
-                                              height: 40.0,
-                                              child: RaisedButton(
-                                                  shape:
-                                                      new RoundedRectangleBorder(
-                                                    borderRadius:
-                                                        new BorderRadius
-                                                            .circular(10.0),
-                                                    side: BorderSide(
-                                                      width: 1.0,
-                                                      color: Colors.indigo[900],
-                                                    ),
-                                                  ),
-                                                  color: Colors.white,
-                                                  textColor: Theme.of(context)
-                                                      .primaryColorDark,
-                                                  child: setUpButtonChild(),
-                                                  onPressed: () {
-                                                    _submitForm();
-                                                  })),
-                                        ),
-                                        Expanded(
-                                          flex: 4,
-                                          child: Container(),
-                                        ),
-                                      ],
+                                    Expanded(
+                                      flex: 8,
+                                      child: ButtonTheme(
+                                          height: 50.0,
+                                          child: RaisedButton(
+                                              shape: new RoundedRectangleBorder(
+                                                borderRadius:
+                                                    new BorderRadius.circular(
+                                                        25.0),
+                                                side: BorderSide(
+                                                  width: 0.0,
+                                                ),
+                                              ),
+                                              color: Color(0xff2079FF),
+                                              textColor: Theme.of(context)
+                                                  .primaryColorDark,
+                                              child: setUpButtonChild(),
+                                              onPressed: () {
+                                                _submitForm();
+                                              })),
                                     ),
-                                  ]))),
+                                    Expanded(
+                                      flex: 1,
+                                      child: Container(),
+                                    ),
+                                  ],
+                                ),
+                                Container(height: 10.0),
+                                CommonWidgets.getDotIndicator(10, 10, 20),
+                              ],
+                            ),
+                          ),
                         ),
                       ),
-                    ],
-                  ),
-                ));
-          }),
-        ]));
+                    ),
+                  ],
+                ),
+              ));
+        }));
   }
 
   void _submitForm() {
@@ -277,19 +303,31 @@ class _SignUpNameUser extends State<SignUpName> {
 
   Widget setUpButtonChild() {
     if (_progressCircleState == 0) {
-      return new Text(
-        "Let's Go!",
-        style: const TextStyle(
-            color: Colors.indigo,
-            fontSize: 16.0,
-            fontFamily: 'Montserrat',
-            fontWeight: FontWeight.w700),
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "CREATE ACCOUNT",
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 16.0,
+              fontFamily: 'Roboto',
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          new Spacer(),
+          Icon(
+            Icons.arrow_forward_ios,
+            color: Colors.white,
+          ),
+        ],
       );
     } else if (_progressCircleState == 1) {
       return Container(
-        margin: EdgeInsets.all(3.0),
+        margin: EdgeInsets.all(5.0),
         child: CircularProgressIndicator(
-          valueColor: AlwaysStoppedAnimation<Color>(Colors.indigo[900]),
+          valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
         ),
       );
     } else {
--- simpliflat/lib/screens/widgets/common.dart
+++ simpliflat/lib/screens/widgets/common.dart
@@ -77,4 +77,39 @@ class CommonWidgets {
     );
   }
 
+  static getDotIndicator(double s1, double s2, double s3) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Color(0xffBFDAFF),
+            shape: BoxShape.circle,
+          ),
+          height: s1,
+          width: s1,
+        ),
+        Container(width: 10.0,),
+        Container(
+          decoration: BoxDecoration(
+            color: Color(0xffBFDAFF),
+            shape: BoxShape.circle,
+          ),
+          height: s2,
+          width: s2,
+        ),
+        Container(width: 10.0,),
+        Container(
+          decoration: BoxDecoration(
+            color: Color(0xffBFDAFF),
+            shape: BoxShape.circle,
+          ),
+          height: s3,
+          width: s3,
+        ),
+      ],
+    );
+  }
+
 }
